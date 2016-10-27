3f66a204a292bf112d84aeb2b0e2a24f7674a9d6
Bogdan Ghervan <bogdan.ghervan@gmail.com>
2016-10-27 18:20:06 +0300

    Refactor exceptions handler to use RespondsWithJson trait.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -10,9 +10,12 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
+use App\Http\RespondsWithJson;
 
 class Handler extends ExceptionHandler
 {
+    use RespondsWithJson;
+
     /**
      * A list of the exception types that should not be reported.
      *
@@ -48,54 +51,32 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $e)
     {
         switch (true) {
-            // Handle validation errors
-            case $e instanceof ValidationException:
-                $validation = $e->validator;
-                return response()->json([
-                    'error' => [
-                        'code' => 'InvalidParameter',
-                        'message' => $validation->errors()->first(),
-                        'param' => $validation->errors()->first(null, ':key')
-                    ]
-                ], 400);
-
             // Route is not found, or route parameters invalid, or abort(404)
             case $e instanceof NotFoundHttpException:
-                return response()->json([
-                    'error' => [
-                        'code' => 'ResourceNotFound',
-                        'message' => 'Resource not found.'
-                    ]
-                ], 405);
+                return $this->errorNotFound();
 
             // Route exists, but is called with the wrong HTTP verb
             case $e instanceof MethodNotAllowedHttpException:
-                return response()->json([
-                    'error' => [
-                        'code' => 'MethodNotAllowed',
-                        'message' => 'Requested method is not supported.'
-                    ]
-                ], 405);
+                return $this->respondWithError('Requested method is not supported.', 'MethodNotAllowed')
+                    ->setStatusCode(405);
+
+            // Handle validation errors
+            case $e instanceof ValidationException:
+                return $this->errorInvalidParameter(
+                    $e->validator->errors()->first(null, ':key'),
+                    $e->validator->errors()->first()
+                );
 
             // Handle all other abort helper calls, however, the developer
             // should rather use more specialized methods for erroring out
             case $e instanceof HttpException:
-                return response()->json([
-                    'error' => [
-                        'code' => 'GeneralError',
-                        'message' => $e->getMessage()
-                    ]
-                ], $e->getStatusCode());
+                return $this->respondWithError($e->getMessage(), 'GeneralError')
+                    ->setStatusCode($e->getStatusCode());
 
             // Handle everything else
             default:
-                return response()->json([
-                    'error' => [
-                        'code' => 'InternalError',
-                        'message' => 'The server encountered an internal error. Please retry your request or '
-                            . 'reach out to us for support.'
-                    ]
-                ], 500);
+                return $this->respondWithError('The server encountered an internal error.', 'InternalError')
+                    ->setStatusCode(500);
         }
     }
 }
